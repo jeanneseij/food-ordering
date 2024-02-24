1d62ea6dfcf00166c969acf53a137db5dc9dbac8
jeanneseij <97769244+jeanneseij@users.noreply.github.com>
2024-02-24 21:00:51 +0800

    Nested Navigation

---
+++ src/app/(tabs)/menu/index.tsx
@@ -0,0 +1,16 @@
+import { View } from '@/components/Themed';
+import { FlatList } from 'react-native';
+import products from '@assets/data/products'
+import ProductListItem from '@/components/ProdcutListItem';
+
+export default function MenuScreen() {
+  return (
+    <FlatList 
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={ item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap:10, padding: 10 }}
+      columnWrapperStyle={{ gap:10 }}
+    />
+  );
+}
