aaeaf8e70d992867ee36e3a333dd03bc6886c218
jeanneseij <97769244+jeanneseij@users.noreply.github.com>
2024-02-24 20:23:14 +0800

    Scrollable component
    modify spacing and column

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
 import { View } from '@/components/Themed';
-import products from '../../../assets/data/products'
+import { FlatList } from 'react-native';
+import products from '@assets/data/products'
 import ProductListItem from '@/components/ProdcutListItem';
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
-    </View>
+    <FlatList 
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={ item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap:10, padding: 10 }}
+      columnWrapperStyle={{ gap:10 }}
+    />
   );
 }
