eedbf07a1487f536a79fedfd694e9d3c22252cd0
jeanneseij <97769244+jeanneseij@users.noreply.github.com>
2024-02-24 20:05:19 +0800

    path config

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import { View } from '@/src/components/Themed';
+import { View } from '@/components/Themed';
 import products from '../../../assets/data/products'
-import ProductListItem from '@/src/components/ProdcutListItem';
+import ProductListItem from '@/components/ProdcutListItem';
 
 export default function MenuScreen() {
   return (
