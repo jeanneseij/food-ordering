395fbad96747e169919bf613081a34df78c83390
jeanneseij <97769244+jeanneseij@users.noreply.github.com>
2024-02-25 14:41:44 +0800

    Cart Checkout Button and Total Cost Calculation

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,18 +3,21 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useCart } from '@/providers/CartProvider'
 import CartListItem from '@/components/CartListItem'
+import Button from '@/components/Button'
 
 const CartScreen = () => {
-  const { items } = useCart()
+  const { items, total } = useCart()
     
   return (
-    <View>
+    <View style={{flex: 1}}>
       {/* <Text>Cart Items Length: {items.length}</Text> */}
       <FlatList 
         data={items}
         renderItem={({item}) => <CartListItem cartItem={item} />}
         contentContainerStyle={{gap: 10, padding: 10}}
       />
+      <Text style={{marginTop: 20, fontSize: 20, fontWeight: '500'}}> Total: ${total}</Text>
+      <Button text="Checkout" style={{marginTop: 'auto'}} />
 
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
